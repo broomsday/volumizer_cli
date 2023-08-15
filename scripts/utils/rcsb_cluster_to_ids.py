64061c3e422926e6d9a9072f2f278c22c40ecdca
Aron Broom <broomsday@gmail.com>
2023-08-15 13:16:57 -0400

    Update RCSB cluster ID getting utility script

--- scripts/utils/rcsb_cluster_to_ids.py
+++ scripts/utils/rcsb_cluster_to_ids.py
@@ -9,14 +9,21 @@ from pathlib import Path
 import typer
 
 
-def main(cluster_file: Path, output_file: Path) -> None:
+def main(
+    cluster_file: Path= typer.Argument(..., help="Sequence ID cluster file downloaded frome RCSB"),
+    output_file: Path= typer.Argument(..., help="Output text file listing RCSB IDs matching first entry of each cluster"),
+) -> None:
     """
     Open an RCSB cluster file and generate a text file with one ID per line,
     where each ID is the first ID in the cluster.
     """
 
     with open(cluster_file, mode="r", encoding="utf-8") as file_in:
-        pdbs = {f"{line.split()[0][:4]}\n" for line in file_in.readlines()}
+        pdbs = {
+            f"{line.split()[0].split('_')[0]}\n"
+            for line in file_in.readlines()
+            if len(line.split()[0].split('_')[0]) == 4
+        }
 
     with open(output_file, mode="w", encoding="utf-8") as file_out:
         file_out.writelines(pdbs)
